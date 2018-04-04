ef7272e78ea573a5670aed5aad0da656839738a8
kevin <kevinlopez34430@hotmail.fr>
2018-04-04 17:34:10 +0200

    Initial commit.

---
+++ test/monapplication2/MonApplication2Test.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monapplication2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author k.lopez
+ */
+public class MonApplication2Test {
+    
+    public MonApplication2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of min method, of class MonApplication2.
+     */
+    @Test
+    public void testMin() {
+        System.out.println("min");
+        String[] args = null;
+        MonApplication2.min(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
